3e4c3c374d837fe0c498ee661403fc689ffe1712
AlbertoGarciaMiguelez <79752037+AlbertoGarciaMiguelez@users.noreply.github.com>
2021-11-25 19:28:42 +0000

    Retoques del 1-10 actualización del 11-20
    Completado

---
+++ AlbertoGarciaMiguelez.sh
@@ -0,0 +1,108 @@
+#1
+
+ls /bin
+
+#2
+
+ls /tmp
+
+#3
+
+mkdir ect
+cd ect
+mkdir tyyaa
+mkdir tqwer
+cd ..
+ls ect/t*
+
+#4
+
+mkdir dev
+cd dev
+mkdir tyyaa
+cd ..
+ls dev/tyy??
+
+#5
+
+cd dev
+mkdir tyy1
+touch tyy3.text
+cd ..
+ls dev/tty*[1-4]
+
+#6
+
+mkdir -v dev/tqweC1
+ls dev/t*c1
+
+#7
+
+ls –a /
+
+#8
+
+mkdir -v ect/ee
+ls –d etc/[^t]*
+
+#9
+
+ls –R /usr
+
+#10
+
+cd /tmp 
+mkdir PRUEBA
+
+#11
+
+pwd
+
+#12
+
+date
+
+#13
+
+cd /home
+
+#14
+
+pwd
+
+#15
+
+ls -i
+
+#16
+
+rm -rf PRUEBA/*
+
+#17
+
+mkdir PRUEBA/dir1
+mkdir PRUEBA/dir1/dir11
+mkdir PRUEBA/dir2
+mkdir PRUEBA/dir3
+mkdir PRUEBA/dir3/dir31
+mkdir PRUEBA/dir3/dir31/dir311
+mkdir PRUEBA/dir3/dir31/dir312
+
+#18
+
+sudo touch /etc/motd PRUEBA/mensaje
+
+#19
+
+cd PRUEBA
+cp mensaje dir1/mensaje && cp mensaje dir2/mensaje && cp mensaje dir3/mensaje
+
+#20
+
+ls -R PRUEBA
+
+#21
+#22
+#23
+#24
+#25
